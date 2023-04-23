414da714b2b8df55e7849d0c0f0c61233efbfc62
IzaanArain <izaansaquib@gmail.com>
2023-04-23 22:43:23 +0500

    adding functionali

---
+++ grid.js
@@ -0,0 +1,93 @@
+//Calculator Logic
+
+//getting Number
+const num0=document.getElementById("0")
+const num1=document.getElementById("1")
+const num2=document.getElementById("2")
+const num3=document.getElementById("3")
+const num4=document.getElementById("4")
+const num5=document.getElementById("5")
+const num6=document.getElementById("6")
+const num7=document.getElementById("7")
+const num8=document.getElementById("8")
+const num9=document.getElementById("9")
+
+//Operators and symbols
+const add=document.getElementById("+")
+const sub=document.getElementById("-")
+const div=document.getElementById("/")
+const mul=document.getElementById("x")
+const eq=document.getElementById("=")
+const dec=document.getElementById(".")
+const reset=document.getElementById("reset")
+//Input field
+const numInput=document.getElementById("numInput")
+// console.log(one.textContent)
+// console.log(num2)
+// console.log(parseFloat(num2)+parseFloat(num1)
+// console.log(add.textContent)
+
+num0.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num0.textContent)
+})
+num1.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num1.textContent)
+})
+
+num2.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num2.textContent)
+})
+num3.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num3.textContent)
+})
+
+num4.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num4.textContent)
+})
+num5.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num5.textContent)
+})
+num6.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num6.textContent)
+})
+num7.addEventListener("click",()=>{
+    numInput.value+=num7.textContent
+})
+num8.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num8.textContent)
+})
+num9.addEventListener("click",()=>{
+    numInput.value+=parseFloat(num9.textContent)
+})
+
+//reset button
+reset.addEventListener('click',()=>{
+    numInput.value=null;
+})
+
+//operators
+let op=""
+add.addEventListener('click',()=>{
+    numInput.value+=add.textContent
+    op="+"
+})
+sub.addEventListener('click',()=>{
+    numInput.value+=sub.textContent
+    op="-"
+})
+mul.addEventListener('click',()=>{
+    numInput.value+=mul.textContent
+    op="x"
+})
+div.addEventListener('click',()=>{
+    numInput.value+=div.textContent
+    op="/"
+})
+
+//decimal
+dec.addEventListener('click',()=>{
+    numInput.value+=dec.textContent
+})
+
+
+  
